05b9e9476c5b904edec23c12b952f4ff09fc4656
Vitaly <viketvova@gmail.com>
2019-12-08 19:49:15 +0200

    Add map filter

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,44 +1,56 @@
-import React from 'react';
-import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
-const DialogItem = (props) => {
-    return (
-        //Вынести NavLink можно создав переменную: let path=`/dialogs/${props.id}` и в NavLink заменить на path
-        <div className={s.dialog}>
-            <NavLink to={`/dialogs/${props.id}`}>
-                {props.name}
-            </NavLink>
-        </div>
-    )
-}
-const Message = (props) => {
-    return (
-    <div className={s.message}>{props.message}</div>
-    )
-}
+const DialogItem = props => {
+  return (
+    //Вынести NavLink можно создав переменную: let path=`/dialogs/${props.id}` и в NavLink заменить на path
+    <div className={s.dialog}>
+      <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+    </div>
+  );
+};
+const Message = props => {
+  return <div className={s.message}>{props.message}</div>;
+};
 
-const Dialogs = (props) => {
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogItems}>
+const Dialogs = props => {
+  let dialogsData = [
+    { id: 1, name: "Dymych" },
+    { id: 2, name: "Andrew" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Sasha" },
+    { id: 5, name: "Vova" }
+  ];
 
-               <DialogItem name="Dimych" id="1"/>
-               <DialogItem name="Vetal" id="2"/>
-               <DialogItem name="Roma" id="3"/>
-               <DialogItem name="Mashs" id="4"/>
-               <DialogItem name="Vova" id="5"/>
+  let messagesData = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How r u" },
+    { id: 3, message: "How is your" },
+    { id: 4, message: "Yo" },
+    { id: 5, message: "Yo" }
+  ];
+  let postData = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How r u" },
+    { id: 3, message: "How is your" },
+    { id: 4, message: "Yo" },
+    { id: 5, message: "Yo" }
+  ];
+  let dialogsElements = dialogsData.map(el => {
+    return <DialogItem name={el.name} id={el.id} />;
+  });
+  let messagesElements = messagesData.map(el => {
+    return <Message message={el.message} />;
+  });
 
-            </div>
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>{dialogsElements}</div>
 
-
-            <div className={s.messages}>
-                <Message message="Hi"/>
-                <Message message="How r u"/>
-                <Message message="How is your> Yo"/>
-                </div>
-        </div>
-    );
-}
+      <div className={s.messages}>{messagesElements}</div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import p from './MyPosts.module.css';
-import Post from './Posts/Post';
+import React from "react";
+import p from "./MyPosts.module.css";
+import Post from "./Posts/Post";
 
 const MyPosts = () => {
-    return (
-
-        <div className={p.postsBlock}>
-            <h3>My Posts</h3>
-            <div>
-                <div>
-                    <textarea></textarea>
-                </div>
-                <div>
-                    <button>Add post</button>
-                </div>
-                <div className={p.posts}>
-                    <Post message="Hi, how r u?" likecount="10"/>
-                    <Post message="It's my first post" likecount="15"/>
-                </div>
-            </div>
-            {/* <Post />
-        <Post /> */}
+  let postData = [
+    { id: 1, message: "Hi, how r u?", likecount: 12 },
+    { id: 2, message: "It's my first post", likecount: 10 }
+  ];
+  let postsElements = postData.map(el => {
+    return <Post message={el.message} likecount={el.likecount} />;
+  });
+  return (
+    <div className={p.postsBlock}>
+      <h3>My Posts</h3>
+      <div>
+        <div>
+          <textarea></textarea>
         </div>
-
-
-    );
-}
-
+        <div>
+          <button>Add post</button>
+        </div>
+        <div className={p.posts}>{postsElements}</div>
+      </div>
+      {/* <Post />
+        <Post /> */}
+    </div>
+  );
+};
 
 export default MyPosts;
